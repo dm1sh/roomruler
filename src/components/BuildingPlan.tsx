77661df39b4c0c6e2c2f91ad2d641cf7fcbf29a6
dm1sh <me@dmitriy.icu>
2021-09-04 03:53:33 +0300

    Added basic draw function for list of rooms

--- src/components/BuildingPlan.tsx
+++ src/components/BuildingPlan.tsx
@@ -1,27 +1,21 @@
-import { RoomT } from "../types/room";
+import { useRoomContext } from "../context";
 import { Canvas } from "./Canvas";
 
 export type BuildingPlanProps = { width: number; height: number };
 
 export const BuildingPlan = ({ width, height }: BuildingPlanProps) => {
-  const room: RoomT = {
-    coordinates: { x: 100, y: 200 },
-    free: true,
-    size: { h: 100, w: 200 },
-  };
+  const { state } = useRoomContext();
 
   return (
     <Canvas
       height={height}
       width={width}
       draw={(ctx) => {
-        ctx.fillStyle = room.free ? "green" : "gray";
-        ctx.fillRect(
-          room.coordinates.x,
-          room.coordinates.y,
-          room.size.w,
-          room.size.h
-        );
+        state.map.map(({ coordinates, size }, index) => {
+          const { free } = state.char[index];
+          ctx.fillStyle = free ? "green" : "gray";
+          ctx.fillRect(coordinates.x, coordinates.y, size.w, size.h);
+        });
       }}
     />
   );
